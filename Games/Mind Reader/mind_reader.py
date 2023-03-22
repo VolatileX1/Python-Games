109d21109ed61cf547166e6267a59168e1581f38
VolatileX1 <128452078+VolatileX1@users.noreply.github.com>
2023-03-22 12:17:29 +0530

    Introducing time limits
    - In this modified version, two new variables are added : "time_limit"and "start_time". The time_limit variable sets the maximum amount of time allowed for the player to guess the number (in seconds). The start_time variable stores the current time when the game starts.
    
    The "while" loop now contains an additional condition that checks whether the time has exceeded the time limit. If so, the game ends immediately, and the player loses. If the guess is correct before the time limit expires, the game ends, and the player wins.

--- Games/Mind Reader/mind_reader.py
+++ Games/Mind Reader/mind_reader.py
@@ -1,7 +1,12 @@
 import random
+import time
 
 # Generate a random number between 1 and 10
 number = random.randint(1, 10)
+
+# Set the time limit to 10 seconds
+time_limit = 10
+start_time = time.time()
 
 # Ask the user to guess the number
 guess = int(input("Guess a number between 1 and 10: "))
@@ -12,8 +17,8 @@ num_guesses = 1
 # Set initial score to 100
 score = 100
 
-# Loop until the user guesses correctly or uses all their guesses
-while guess != number and num_guesses < 3:
+# Loop until the user guesses correctly or uses all their guesses or runs out of time
+while guess != number and num_guesses < 3 and (time.time() - start_time) < time_limit:
     if guess < number:
         print("Too low!")
         if number % 2 == 0:
@@ -36,9 +41,12 @@ while guess != number and num_guesses < 3:
     # Reduce the score with each incorrect guess
     score -= 10
     
-# Check if the user guessed the number
+# Check if the user guessed the number or ran out of time
 if guess == number:
     print("Congratulations! You guessed the number in", num_guesses, "guesses.")
+    print("Your score is:", score)
+elif (time.time() - start_time) >= time_limit:
+    print("Sorry, you ran out of time. The number was", number)
     print("Your score is:", score)
 else:
     print("Sorry, you ran out of guesses. The number was", number)
