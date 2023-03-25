84f9fc542c68e2f3cd7802d2f8f1b8295614b8f7
VolatileX1 <128452078+VolatileX1@users.noreply.github.com>
2023-03-25 12:18:35 +0530

    Adding Scoring system
    - Assigning points for each checkpoint reached and subtract points for each move made.

--- Robomaze/Robomaze.py
+++ Robomaze/Robomaze.py
@@ -26,8 +26,13 @@ checkpoints = [
     (MAZE_SIZE-2, MAZE_SIZE-2)
 ]
 
+# Define initial score and moves left
+score = 0
+num_moves_left = 50
+
 # Define a function to print the maze with the robot's position
 def print_maze():
+    global score
     for row in range(len(maze)):
         for col in range(len(maze[row])):
             if row == robot_row and col == robot_col:
@@ -46,26 +51,36 @@ def print_maze():
             else:
                 print(maze[row][col], end="")
         print()
+    print("Score:", score)
+    print("Moves left:", num_moves_left)
 
 # Define a function to move the robot forward
 def move_forward():
-    global robot_row, robot_col
+    global robot_row, robot_col, score, num_moves_left
     if robot_dir == "up":
         if maze[robot_row-1][robot_col] == " ":
             robot_row -= 1
+            score -= 1 # Subtract 1 point for each move
+            num_moves_left -= 1
     elif robot_dir == "down":
         if maze[robot_row+1][robot_col] == " ":
             robot_row += 1
+            score -= 1 # Subtract 1 point for each move
+            num_moves_left -= 1
     elif robot_dir == "left":
         if maze[robot_row][robot_col-1] == " ":
             robot_col -= 1
+            score -= 1 # Subtract 1 point for each move
+            num_moves_left -= 1
     elif robot_dir == "right":
         if maze[robot_row][robot_col+1] == " ":
             robot_col += 1
+            score -= 1 # Subtract 1 point for each move
+            num_moves_left -= 1
 
 # Define a function to turn the robot left
 def turn_left():
-    global robot_dir
+    global robot_dir, score, num_moves_left
     if robot_dir == "up":
         robot_dir = "left"
     elif robot_dir == "down":
@@ -74,10 +89,12 @@ def turn_left():
         robot_dir = "down"
     elif robot_dir == "right":
         robot_dir = "up"
+    score -= 1 # Subtract 1 point for each turn
+    num_moves_left -= 1
 
 # Define a function to turn the robot right
 def turn_right():
-    global robot_dir
+    global robot_dir, score, num_moves_left
     if robot_dir == "up":
         robot_dir = "right"
     elif robot_dir == "down":
@@ -86,9 +103,10 @@ def turn_right():
         robot_dir = "up"
     elif robot_dir == "right":
         robot_dir = "down"
+    score -= 1 # Subtract 1 point for each turn
+    num_moves_left -= 1
 
 # Play the game until the robot reaches all checkpoints or runs out of moves
-num_moves_left = 50 # Set a limit for the number of moves
 checkpoints_reached = set()
 while checkpoints_reached != set(checkpoints) and num_moves_left > 0:
     print_maze()
@@ -96,16 +114,20 @@ while checkpoints_reached != set(checkpoints) and num_moves_left > 0:
     user_input = input()
     if user_input == "f":
         move_forward()
-        num_moves_left -= 1
         if (robot_row, robot_col) in checkpoints:
+            # Add 10 points for each checkpoint reached
+            score += 10
             checkpoints_reached.add((robot_row, robot_col))
     elif user_input == "l":
         turn_left()
     elif user_input == "r":
         turn_right()
+    else:
+        print("Invalid input. Please try again.")
+    print_maze()
 
-# Determine the outcome of the game
 if checkpoints_reached == set(checkpoints):
-    print("Congratulations, you have reached all checkpoints!")
+    print("Congratulations! You reached all the checkpoints!")
 else:
-    print("Game over, you have run out of moves or failed to reach all checkpoints.")
+    print("Game over. Better luck next time!")
+print(f"Your final score is {score}.")
