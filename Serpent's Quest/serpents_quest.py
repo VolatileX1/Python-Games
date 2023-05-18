7b96569cbd3eff81545cb741b228a94e2145288a
VolatileX1 <128452078+VolatileX1@users.noreply.github.com>
2023-05-18 09:59:55 +0530

    Cleanup

--- Serpent's Quest/serpents_quest.py
+++ Serpent's Quest/serpents_quest.py
@@ -1,31 +1,24 @@
 import pygame
 import random
 
-# initialize Pygame
 pygame.init()
 
-# set up game window
-window_width = 800
-window_height = 600
-game_window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption('Unique Snake Game')
+window_size = (800, 600)
+game_window = pygame.display.set_mode(window_size)
+pygame.display.set_caption('Serpent\'s Quest')
 
-# set up game variables
 snake_size = 10
 food_size = 15
 score = 0
 
-# define colors
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 green = (0, 255, 0)
 
-# define fonts
 font_40 = pygame.font.SysFont(None, 40)
 font_60 = pygame.font.SysFont(None, 60)
 
-# define functions
 def draw_snake(snake_list):
     for x_and_y in snake_list:
         pygame.draw.rect(game_window, green, [x_and_y[0], x_and_y[1], snake_size, snake_size])
@@ -39,32 +32,27 @@ def display_score(score):
 
 def display_game_over_message():
     game_over_text = font_60.render('Game Over!', True, white)
-    game_window.blit(game_over_text, [window_width/2 - game_over_text.get_width()/2, window_height/2 - game_over_text.get_height()/2])
+    game_window.blit(game_over_text, [window_size[0]/2 - game_over_text.get_width()/2, window_size[1]/2 - game_over_text.get_height()/2])
     pygame.display.update()
     pygame.time.wait(2000)
 
-# set up game loop
 clock = pygame.time.Clock()
 
 def game_loop():
 
-    # set up initial snake position and movement
-    start_x = random.randrange(0, window_width - snake_size, 10)
-    start_y = random.randrange(0, window_height - snake_size, 10)
+    start_x = random.randrange(0, window_size[0] - snake_size, 10)
+    start_y = random.randrange(0, window_size[1] - snake_size, 10)
     snake_list = [[start_x, start_y]]
     direction = 'right'
 
-    # set up initial food position
-    food_position = [random.randrange(food_size, window_width - food_size), random.randrange(food_size, window_height - food_size)]
+    food_position = [random.randrange(food_size, window_size[0] - food_size), random.randrange(food_size, window_size[1] - food_size)]
 
-    # start game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-            # change direction based on arrow key inputs
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT and direction != 'right':
                     direction = 'left'
@@ -75,7 +63,6 @@ def game_loop():
                 elif event.key == pygame.K_DOWN and direction != 'up':
                     direction = 'down'
 
-        # move snake
         if direction == 'right':
             new_x = snake_list[-1][0] + snake_size
             new_y = snake_list[-1][1]
@@ -89,35 +76,27 @@ def game_loop():
             new_x = snake_list[-1][0]
             new_y = snake_list[-1][1] - snake_size
 
-        # check if snake hits wall or self
-        if new_x >= window_width or new_x < 0 or new_y >= window_height or new_y < 0 or [new_x, new_y] in snake_list[:-1]:
+        if new_x >= window_size[0] or new_x < 0 or new_y >= window_size[1] or new_y < 0 or [new_x, new_y] in snake_list[:-1]:
             display_game_over_message()
             game_loop()
 
-        # check if snake eats food
         if abs(new_x - food_position[0]) < food_size and abs(new_y - food_position[1]) < food_size:
             score += 1
             snake_list.append([new_x, new_y])
-            food_position = [random.randrange(food_size, window_width - food_size), random.randrange(food_size, window_height - food_size)]
-
+            food_position = [random.randrange(food_size, window_size[0] - food_size), random.randrange(food_size, window_size[1] - food_size)]
         else:
             snake_list.append([new_x, new_y])
             del snake_list[0]
 
-        # draw game elements
         game_window.fill(black)
         draw_snake(snake_list)
         draw_food(food_position)
         display_score(score)
 
-        # update game window
         pygame.display.update()
 
-        # set game speed
         clock.tick(10)
 
-# start game
 game_loop()
 
-# quit Pygame
 pygame.quit()
